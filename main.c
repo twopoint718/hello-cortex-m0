844c16fe145d4b6dda3c8514c1bd6f91e527225f
Chris Wilson <christopher.j.wilson@gmail.com>
2019-10-15 22:34:17 -0500

    Remove unnecessary function

--- main.c
+++ main.c
@@ -3,14 +3,6 @@
 #define LED ((unsigned int) 5)
 
 /*
- * Initialize the SysTick registers to provide a roughly 1-second interval
- * interrupt.
- */
-void initialize_systick_timer(void) {
-    SysTick_Config(500000);
-}
-
-/*
  * When the SysTick timer hits zero, Toggle the LED.
  */
 void SysTick_Handler(void) {
@@ -19,7 +11,7 @@ void SysTick_Handler(void) {
 
 void main(void) {
     REG_PORT_DIR0 |= (1<<LED);  // Set to output
-    initialize_systick_timer(); // Start the SysTick timer/interrupt
+    SysTick_Config(1000000);     // Initialize the SysTick registers
 
     // Don't need to do anything, the interrupt will handle it
     while (1) { }
